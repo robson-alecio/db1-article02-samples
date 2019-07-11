8cba0061f5add21605b79721ff2e36af65b9c004
Robson Alecio Somera <robson.somera@db1.com.br>
2019-07-11 08:17:00 -0300

    Change HolidayService tests to parametrized tests

--- src/test/kotlin/db1/article02/dates/HolidayServiceTest.kt
+++ src/test/kotlin/db1/article02/dates/HolidayServiceTest.kt
@@ -3,32 +3,49 @@ package db1.article02.dates
 import assertk.assertThat
 import assertk.assertions.isFalse
 import assertk.assertions.isTrue
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.provider.MethodSource
 import java.time.LocalDate
 import java.time.Month
+import java.util.stream.Stream
 
 internal class HolidayServiceTest {
 
     private var holidayService: HolidayService = HolidayService()
 
-    @Test
-    fun `regular day`() {
-        assertThat(holidayService.existFor(LocalDate.of(2019, Month.SEPTEMBER, 6))).isFalse()
-        assertThat(holidayService.existFor(LocalDate.of(2019, Month.SEPTEMBER, 8))).isFalse()
-        assertThat(holidayService.existFor(LocalDate.of(2019, Month.DECEMBER, 24))).isFalse()
-        assertThat(holidayService.existFor(LocalDate.of(2019, Month.DECEMBER, 26))).isFalse()
-        assertThat(holidayService.existFor(LocalDate.of(2019, Month.DECEMBER, 31))).isFalse()
-        assertThat(holidayService.existFor(LocalDate.of(2020, Month.JANUARY, 2))).isFalse()
+    companion object {
+        @JvmStatic
+        fun regularDayInput(): Stream<LocalDate> = Stream.of(
+            LocalDate.of(2019, Month.SEPTEMBER, 6),
+            LocalDate.of(2019, Month.SEPTEMBER, 8),
+            LocalDate.of(2019, Month.DECEMBER, 24),
+            LocalDate.of(2019, Month.DECEMBER, 26),
+            LocalDate.of(2019, Month.DECEMBER, 31),
+            LocalDate.of(2020, Month.JANUARY, 2)
+        )
+
+        @JvmStatic
+        fun knownHolidaysInput(): Stream<LocalDate> = Stream.of(
+            LocalDate.of(2019, Month.SEPTEMBER, 7),
+            LocalDate.of(2019, Month.DECEMBER, 25),
+            LocalDate.of(2020, Month.JANUARY, 1),
+            LocalDate.of(2020, Month.SEPTEMBER, 7),
+            LocalDate.of(2020, Month.DECEMBER, 25),
+            LocalDate.of(2021, Month.JANUARY, 1)
+        )
     }
 
-    @Test
-    fun `known holidays`() {
-        assertThat(holidayService.existFor(LocalDate.of(2019, Month.SEPTEMBER, 7))).isTrue()
-        assertThat(holidayService.existFor(LocalDate.of(2019, Month.DECEMBER, 25))).isTrue()
-        assertThat(holidayService.existFor(LocalDate.of(2020, Month.JANUARY, 1))).isTrue()
-        assertThat(holidayService.existFor(LocalDate.of(2020, Month.SEPTEMBER, 7))).isTrue()
-        assertThat(holidayService.existFor(LocalDate.of(2020, Month.DECEMBER, 25))).isTrue()
-        assertThat(holidayService.existFor(LocalDate.of(2021, Month.JANUARY, 1))).isTrue()
+    @ParameterizedTest
+    @MethodSource("regularDayInput")
+    fun `regular day`(date: LocalDate) {
+        assertThat(holidayService.existFor(date)).isFalse()
+    }
+
+    @ParameterizedTest
+    @MethodSource("knownHolidaysInput")
+    fun `known holidays`(date: LocalDate) {
+        assertThat(holidayService.existFor(date)).isTrue()
     }
 
 }
